9e127e96388d9168449b2aee495ef8243facbade
AliAbbas <aliabbas@cmail.carleton.ca>
2023-11-30 16:35:10 +0500

    commit 1 - Add tailwind css 2 - Add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'],
+  devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
