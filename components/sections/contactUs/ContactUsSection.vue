55329eac22fb2a4ec9110bbe8d817552d54253ee
AliAbbas <aliabbas@cmail.carleton.ca>
2023-12-21 15:41:59 +0500

    Contatc us

--- components/sections/contactUs/ContactUsSection.vue
+++ components/sections/contactUs/ContactUsSection.vue
@@ -3,9 +3,9 @@ import NextButton from "~/components/general/NextButton.vue";
 </script>
 <template>
   <div
-    class="container lg:w-4/6 xl:w-3/6 pt-[6rem] text-center flex flex-col items-center justify-center"
+    class="container lg:w-4/6 xl:w-3/6 lg:py-[6rem] py-4 text-center flex flex-col items-center justify-center"
   >
-    <div class="text-3xl mb-10">email us</div>
+    <div class="text-3xl mt-10 lg:mt-0 lg:mb-10">email us</div>
     <div class="w-3/4 lg:w-2/4 flex flex-col">
       <u-input
         variant="outline"
@@ -31,13 +31,12 @@ import NextButton from "~/components/general/NextButton.vue";
       <u-textarea
         variant="outline"
         placeholder="message"
-        rows="8"
         size="3xl"
         class="my-4"
-        textarea-class="rounded-2xl bg-secondary-bg"
+        textarea-class="rounded-2xl bg-secondary-bg h-[100px] lg:h-[250px]"
       />
-      <div class="w-full flex flex-col mt-12 items-center justify-center">
-        <NextButton class="w-2/6 my-3" />
+      <div class="w-full flex flex-col lg:mt-12 items-center justify-center">
+        <NextButton class="w-2/6 mb-10 lg:mb-0 lg:my-3" />
       </div>
     </div>
   </div>
